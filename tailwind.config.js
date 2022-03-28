c0dc0447e36039fcd701b9139565b24339021d9d
rnb2022 <mdb1710@gmail.com>
2022-03-28 11:27:56 -0400

    added base home components

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
